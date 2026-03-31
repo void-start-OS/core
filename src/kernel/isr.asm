5d38b754faa0aaa3faf56a9e4065cc3ae88985a8
frisk-V5 <yuniaochaoyang@gmail.com>
2026-03-31 17:06:02 +0900

    Update isr.asm

--- src/kernel/isr.asm
+++ src/kernel/isr.asm
@@ -1,28 +1,32 @@
 ; src/kernel/isr.asm
+; CPU 例外 ISR スタブ
+; C の exception_handler(uint32_t int_no, uint32_t err_code) を呼び出す
+
 [BITS 32]
 
 extern exception_handler
 
+; 例外番号 n、エラーコードなし
 %macro ISR_NOERR 1
 global isr%1_handler
 isr%1_handler:
-    push dword 0      ; ダミーのエラーコード
-    push dword %1     ; 例外番号
-    jmp isr_common
+    push dword 0          ; err_code = 0
+    push dword %1         ; int_no
+    call exception_handler
+    add esp, 8            ; int_no, err_code を片付ける
+    iret
 %endmacro
 
+; 例外番号 n、エラーコードあり（CPU が err_code をすでに push 済み）
 %macro ISR_ERR 1
 global isr%1_handler
 isr%1_handler:
-    push dword %1     ; 例外番号
-    jmp isr_common
+    push dword %1         ; int_no
+    call exception_handler
+    add esp, 4            ; int_no だけ片付ける（err_code は CPU が push したもの）
+    iret
 %endmacro
 
-isr_common:
-    call exception_handler
-    add esp, 8
-    iret
-
-ISR_NOERR 0   ; Divide by zero
-ISR_ERR   13  ; General protection fault
-ISR_ERR   14  ; Page fault
+ISR_NOERR 0    ; Divide by zero
+ISR_ERR   13   ; General protection fault
+ISR_ERR   14   ; Page fault
